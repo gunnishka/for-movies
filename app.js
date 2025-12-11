f6a59479a90f67b88f8742c38a694bcdfd72493b
gunnishka <emilganiev06@gmail.com>
2025-12-11 17:28:23 +0300

    Косметические изменения кода

--- app.js
+++ app.js
@@ -4,23 +4,19 @@ const moviesRouter = require("./routes/movies");
 const logger = require("./middleware/logger");
 
 const app = express();
-const PORT = process.env.PORT || 3001; // Измените порт
+const PORT = process.env.PORT || 3001;
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(logger);
 
-// Маршруты
 app.use("/api/movies", moviesRouter);
 
-// Главная страница
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
